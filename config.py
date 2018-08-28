c24a1e3b30a2f472774a48d7053b6ee5f972cfc9
Stepan <sd.denisoffn@gmail.com>
2018-08-28 19:57:51 +0300

    init commit

---
+++ config.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, json
+import vk_api
+
+
+access_token = '5c494f5c880e7325a1f44ecebc85397b6bd65d24ff0d18416d554e83540a9ee8aac5204b5cf7e525ec225'
+confirmation_token = 'c90019ed'
+
+app = Flask(__name__)
+
+session = vk_api.VkApi(token=access_token)
+vk = session.get_api()
---
+++ main.py
@@ -0,0 +1,32 @@
+from config import *
+from datetime import datetime
+import time
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return 'Hello from server!'
+
+
+@app.route('/', methods=['POST'])
+def processing():
+    data = json.loads(request.data)
+    if 'type' not in data.keys():
+        return 'not vk'
+    if data['type'] == 'confirmation':
+        return confirmation_token
+    elif data['type'] == 'message_new':
+        user_id = data['object']['from_id']
+        vk.messages.send(user_id=str(user_id), message=open('greeting.txt', 'r').read())
+    return 'ok'
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            app.run(host='0.0.0.0', port=8080, debug=True)
+        except Exception as e:
+            f = open('errors.txt', 'a', encoding='utf-8')
+            f.write(str(e) + ' ### ' + str(datetime.now()) + '\n')
+            f.close()
+            time.sleep(5)
