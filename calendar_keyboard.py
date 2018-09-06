76fd88b60904c7487b7859bd73f124a38ddb4c19
Stepan <sd.denisoffn@gmail.com>
2018-09-06 21:35:58 +0300

    keyboard_calendar update

--- calendar_keyboard.py
+++ calendar_keyboard.py
@@ -3,6 +3,17 @@ import calendar
 from datetime import datetime
 
 
+weekdays = {
+    0: 'пн',
+    1: 'вт',
+    2: 'ср',
+    3: 'чт',
+    4: 'пт',
+    5: 'сб',
+    6: 'вс'
+}
+
+
 def create_calendar():
     keyboard = VkKeyboard(one_time=True)
     now = datetime.now()
@@ -10,7 +21,8 @@ def create_calendar():
     keyboard.add_line()
     for day in range(1, calendar.monthrange(now.year, now.month)[1] + 1):
         date = str(now.year) + str(now.month).zfill(2) + str(day).zfill(2)
-        keyboard.add_button(label=str(day), color=VkKeyboardColor.DEFAULT, payload={'action': 'calendar', 'date': date})
+        weekday = weekdays[calendar.weekday(now.year, now.month, day)]
+        keyboard.add_button(label=str(day) + ' ' + weekday, color=VkKeyboardColor.DEFAULT, payload={'action': 'calendar', 'date': date})
         if day % 4 == 0:
             keyboard.add_line()
     return keyboard.get_keyboard()
